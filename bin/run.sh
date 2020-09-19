8343141c8f5b8e7085885acfec5e994024459c14
David Haase <davidhenryhaase@aol.com>
2020-09-19 18:50:20 -0400

    Adjusted the relative path

--- bin/run.sh
+++ bin/run.sh
@@ -2,13 +2,13 @@
 
 #Check if the virtual environement already exists
 # FILE=/logos/venv # use this if building docker
-FILE=venv # use this if using Terminal
+FILE=../venv # use this if using Terminal
 if [ -d "$FILE" ]
 
 # venv directory exists already, so do nothing
 then
 #   export FLASK_APP=/logos/logos.py # use this if building docker
-  export FLASK_APP=logos.py # use this if using Terminal
+  export FLASK_APP=../logos.py # use this if using Terminal
   export FLASK_DEBUG=1
   source $FILE/bin/activate
   flask run
