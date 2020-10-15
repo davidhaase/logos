df63b5bcc242babc342fc15c36e4aeeb6eaa545f
David Haase <davidhenryhaase@aol.com>
2020-10-14 23:32:09 -0400

    Updated model table with build_display_name to look more friendly

--- app/main/views.py
+++ app/main/views.py
@@ -18,12 +18,6 @@ from ..translator import Translator
 from ..utils import S3File, seconds_to_string
 
 
-
-
-
-
-# from ..filemanager import create_file
-
 @main.route('/', methods=['GET', 'POST'])
 def index():
     # LOAD VARIABLES for the PAGE in GENERAL
@@ -35,7 +29,7 @@ def index():
     # Populate the user options based on these availabilities
     form = TranslationForm()
     Models = Model()
-    form.form_selection_build.choices = Models.get_distinct('build_name')
+    form.form_selection_build.choices = Models.get_distinct('build_display_name')
     form.form_selection_input_lang.choices = Models.get_distinct('source_lang_en')
     form.form_selection_output_lang.choices =Models.get_distinct('target_lang_en')
 
@@ -47,95 +41,104 @@ def index():
         # Start a timer to see how long it takes to translate
         start_time = datetime.utcnow()
 
-        # Load the data from the form into memory
-        build_name = form.form_selection_build.data
+        # USER DATA FROM FORM
+        # The code below converts user-choices into system values
+        # Unpack the user-chosen values
+        build_display_name = form.form_selection_build.data
+        (engine, subset, epochs) = build_display_name.split(' ')
         input_string = form.form_string_input.data
         source_lang_en = form.form_selection_input_lang.data
         target_lang_en = form.form_selection_output_lang.data
         output_string = ''
         
         # Model names are unique by concatenating the model attributes
-        model = Models.get_model(f'{build_name}_{source_lang_en}_{target_lang_en}')
+        name = f'{engine}_{subset}_{epochs}_{source_lang_en}_{target_lang_en}'
+        model = Models.get_model(name)
         
-        # CHECK FOR CACHED TRANSLATIONS FIRST
-        already_translated = Translations.get_translation(model['model_name'], input_string)
-        if not already_translated:
-            
-            # CHECK FOR LOCAL FILES: model.h5, source_tokenizer.pkl, & target_tokenizer.pkl
-            # Keep in mind that the paths defined in the DB are relative: they start at /data/...
-            # ...so get the full path to the app directory set in the the CONFIG
-            app_dir = current_app.config['APP_DIR']
-            full_path_to_model = f'{app_dir}/{model["model_path"]}'
-            full_path_to_source_tokenizer = f'{app_dir}/{model["source_tokenizer_path"]}'
-            full_path_to_target_tokenizer = f'{app_dir}/{model["target_tokenizer_path"]}'
-
-            # First confirm that the three files exist locally: model.ht, source_tokenize.pkl, target_tokenizer.pkl
-            # If they don't exist, you'll have to fetch them from AWS S3
-            if not os.path.exists(full_path_to_model):
+        if model:
+            # CHECK FOR CACHED TRANSLATIONS FIRST
+            already_translated = Translations.get_translation(model['model_name'], input_string)
+            if not already_translated:
                 
-                # Do a quick check to see if all the partent directories are there first
-                model_dir = os.path.abspath(os.path.dirname(full_path_to_model))
-                if not os.path.exists(model_dir):
-                    os.makedirs(model_dir)
+                # CHECK FOR LOCAL FILES: model.h5, source_tokenizer.pkl, & target_tokenizer.pkl
+                # Keep in mind that the paths defined in the DB are relative: they start at /data/...
+                # ...so get the full path to the app directory set in the the CONFIG
+                app_dir = current_app.config['APP_DIR']
+                full_path_to_model = f'{app_dir}/{model["model_path"]}'
+                full_path_to_source_tokenizer = f'{app_dir}/{model["source_tokenizer_path"]}'
+                full_path_to_target_tokenizer = f'{app_dir}/{model["target_tokenizer_path"]}'
+
+                # First confirm that the three files exist locally: model.ht, source_tokenize.pkl, target_tokenizer.pkl
+                # If they don't exist, you'll have to fetch them from AWS S3
+                if not os.path.exists(full_path_to_model):
+                    
+                    # Do a quick check to see if all the partent directories are there first
+                    model_dir = os.path.abspath(os.path.dirname(full_path_to_model))
+                    if not os.path.exists(model_dir):
+                        os.makedirs(model_dir)
+                    
+                    # Now fetch the files from AWS S3
+                    file_in_cloud = S3File(model['aws_bucket_name'], model['model_path'])
+                    file_in_cloud.copy_from_S3_to(full_path_to_model)
+
+                if not os.path.exists(full_path_to_source_tokenizer):
+                    file_in_cloud = S3File(model['aws_bucket_name'], model['source_tokenizer_path'])
+                    file_in_cloud.copy_from_S3_to(full_path_to_source_tokenizer)
+
+                if not os.path.exists(full_path_to_target_tokenizer):    
+                    file_in_cloud = S3File(model['aws_bucket_name'], model['target_tokenizer_path'])
+                    file_in_cloud.copy_from_S3_to(full_path_to_target_tokenizer)
+
                 
-                # Now fetch the files from AWS S3
-                file_in_cloud = S3File(model['aws_bucket_name'], model['model_path'])
-                file_in_cloud.copy_from_S3_to(full_path_to_model)
+                # This dict() is passed to the Translator model
+                model_prefs = { 'model_path': f'{app_dir}/{model["model_path"]}',
+                                'source_tokenizer' : pickle.load(open(f'{app_dir}/{model["source_tokenizer_path"]}', 'rb')),
+                                'source_word_count' : model['source_word_count'],
+                                'source_max_length' : model['source_max_length'],
+                                'target_tokenizer' : pickle.load(open(f'{app_dir}/{model["target_tokenizer_path"]}', 'rb')),
+                                'target_word_count' : model['target_word_count'],
+                                'target_max_length' : model['target_max_length'] }
 
-            if not os.path.exists(full_path_to_source_tokenizer):
-                file_in_cloud = S3File(model['aws_bucket_name'], model['source_tokenizer_path'])
-                file_in_cloud.copy_from_S3_to(full_path_to_source_tokenizer)
+                # HERE IS THE TRANSLATION PIECE
+                tr = Translator(model_prefs)
+                output_string = tr.translate(input_string)
 
-            if not os.path.exists(full_path_to_target_tokenizer):    
-                file_in_cloud = S3File(model['aws_bucket_name'], model['target_tokenizer_path'])
-                file_in_cloud.copy_from_S3_to(full_path_to_target_tokenizer)
+                # Tag it with the date and duration for curiosity
+                date=datetime.utcnow()
+                elapsed_time = date-start_time
+                session['known'] = False
+                
+                # ADD TO THE DB
+                try:
+                    translation = Translation()
+                    translation_record={
+                        'model_name' : model['model_name'],
+                        'build_display_name' : build_display_name,
+                        'input_string' : input_string,
+                        'source_lang_en' : source_lang_en,
+                        'output_string' : output_string,
+                        'target_lang_en' : target_lang_en,
+                        'duration' : seconds_to_string(elapsed_time.seconds),
+                        'date_created' : date.strftime("%m/%d/%Y, %H:%M:%S")
+                    }
+                    response = translation.put_item(translation_record)
 
+                except Exception as e:
+                    flash(f'Error: not able to add translation to database; {e}') 
             
-            # This dict() is passed to the Translator model
-            model_prefs = { 'model_path': f'{app_dir}/{model["model_path"]}',
-                            'source_tokenizer' : pickle.load(open(f'{app_dir}/{model["source_tokenizer_path"]}', 'rb')),
-                            'source_word_count' : model['source_word_count'],
-                            'source_max_length' : model['source_max_length'],
-                            'target_tokenizer' : pickle.load(open(f'{app_dir}/{model["target_tokenizer_path"]}', 'rb')),
-                            'target_word_count' : model['target_word_count'],
-                            'target_max_length' : model['target_max_length'] }
-
-            # HERE IS THE TRANSLATION PIECE
-            tr = Translator(model_prefs)
-            output_string = tr.translate(input_string)
-
-            # Tag it with the date and duration for curiosity
-            date=datetime.utcnow()
-            elapsed_time = date-start_time
-            session['known'] = False
-            
-            # ADD TO THE DB
-            try:
-                translation = Translation()
-                translation_record={
-                    'model_name' : model['model_name'],
-                    'build_name' : build_name,
-                    'input_string' : input_string,
-                    'source_lang_en' : source_lang_en,
-                    'output_string' : output_string,
-                    'target_lang_en' : target_lang_en,
-                    'duration' : seconds_to_string(elapsed_time.seconds),
-                    'date_created' : date.strftime("%m/%d/%Y, %H:%M:%S")
-                }
-                response = translation.put_item(translation_record)
-
-            except Exception as e:
-                flash(f'Error: not able to add translation to database; {e}') 
+            # # The translation already exists for this model so used the cached version
+            else:
+                session['known'] = True
+                flash(f'Showing cached translation which already exists in the database')
+                output_string = already_translated['output_string']
         
-        # # The translation already exists for this model so used the cached version
-        else:
-            session['known'] = True
-            flash(f'Showing cached translation which already exists in the database')
-            output_string = already_translated['output_string']
-               
+        else: # BIG ERROR, model was not found
+            flash(f'Unable to locate model: {name}')
+            logging.warning(f'User-selected model name not found: {name}')
+
         # SAVE BROWSER SESSION
         # (NB: this is NOT the db session)
-        session['build_name'] = build_name
+        session['build_display_name'] = build_display_name
         session['input_string'] = input_string
         session['source_lang_en'] = source_lang_en
         session['target_lang_en'] = target_lang_en
@@ -148,8 +151,8 @@ def index():
     # ...to the values in the session[] dictionary
     # But the session keys won't exist if it's the first session of the browser, so
     # ...you can set the default values here for a fresh session
-    if 'build_name' in session:
-       form.form_selection_build.data = session['build_name']
+    if 'build_display_name' in session:
+       form.form_selection_build.data = session['build_display_name']
     if 'input_string' in session:
         form.form_string_input.data= session['input_string'] 
     if 'source_lang_en' in session:
@@ -301,7 +304,7 @@ def about():
          
         with open(json_file) as json_file:
             language_list = json.load(json_file)
-        form_list = [Languages.load_items(language_list)]
+        Languages.load_items(language_list)
 
         # BUILD ENGINE TABLE in AWS
         # One-time code to populate Engine table 
@@ -319,7 +322,6 @@ def about():
         # Crawls S3 to see what models are there and derives the tables values from the structure
         Models = Model()
         s3_dir = S3File('logos-models', 'data')
-        form_list = []
         relative_path = 'data/models/'
         list_of_models = s3_dir.crawl_models('data/models')
         for engine in list_of_models:
@@ -327,7 +329,6 @@ def about():
                 for source in list_of_models[engine][build]:
                     for target in list_of_models[engine][build][source]:
                         path = f'{relative_path}{engine}/{build}/{source}/{target}/'
-                        form_list.append(path)
                         source_tokenizer_file = path + 'source_tokenizer.pkl'
                         target_tokenizer_file = path + 'target_tokenizer.pkl'
                         model_path= path + 'model.h5'
@@ -346,36 +347,20 @@ def about():
                         pickled_file = S3File('logos-models', s3_pkl)
                         pickled_file.copy_from_S3_to(target_pkl)
                         model_prefs = pickle.load(open(target_pkl, 'rb'))
-                        # {'model_path': 'models/tr_to_en/basic_75K_35E_fixed/model.h5',
-                        # 'source_tokenizer': <keras_preprocessing.text.Tokenizer object at 0x7fa9c3889160>,
-                        # 'source_max_length': 9,
-                        # 'source_vocab_size': 23521,
-                        # 'target_tokenizer': <keras_preprocessing.text.Tokenizer object at 0x7fa9c3b90588>,
-                        # 'target_vocab_size': 8183,
-                        # 'target_max_length': 7,
-                        # 'total_count': 500237,
-                        # 'train_count': 67500,
-                        # 'test_count': 7500,
-                        # 'BLEU1': 0.6865620717271048,
-                        # 'BLEU2': 0.5824291114596682, 
-                        # 'BLEU3': 0.5292572641080151,
-                        # 'BLEU4': 0.38655408626018345}
                         
-
                         ### End hacks
                         date = datetime.utcnow()
                         
                         model_record = { 
-                            'model_name' : f'{build}_{source}_{target}',
-                            'model_display_name' : f'{engine} {str(sentences)}K subset {str(epochs)} epochs',
+                            'model_name' : f'{engine}_{str(sentences)}K_{str(epochs)}e_{source}_{target}',
+                            'build_display_name': f'{engine} {str(sentences)}K {str(epochs)}e',
+                            'build' : build,
                             'engine' : engine,
-                            'build_name' : build,
-                            'date_created' : date.strftime("%m/%d/%Y, %H:%M:%S"),
                             'source_lang_en' : source,
                             'target_lang_en' : target,
-                            'engine' : engine,
                             'epochs' : epochs,
                             'subset' : sentences,
+                            'date_created' : date.strftime("%m/%d/%Y, %H:%M:%S"),
                             'source_tokenizer_path' : source_tokenizer_file,
                             'source_max_length' : int(model_prefs['source_max_length']),
                             'source_word_count' : int(model_prefs['source_vocab_size']),
@@ -399,12 +384,12 @@ def about():
 
                         try:
                             response = Models.put_item(model_record)
+                            flash(f'Database tables loaded successfully') 
                         except Exception as e:
                             flash(f'unable to load db: {e}') 
-        session['form_list'] = form_list
         return redirect(url_for('.about'))
 
-    return render_template('about.html', form=form, form_list=session.get('form_list'))  
+    return render_template('about.html', form=form)  
 
 
 @main.route('/translationhistory', methods=['GET'])
@@ -418,7 +403,7 @@ def translationhistory():
             translation['source_lang_en'],
             translation['output_string'],
             translation['target_lang_en'],
-            translation['build_name'],
+            translation['build_display_name'],
             translation['duration']
         ]
         for translation in Translations.scan()
